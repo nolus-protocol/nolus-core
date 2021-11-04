0b1e0c3deb43f62f266a17f23e175cbb1d8499dd
Veselin Vlasakiev <veselin.vlasakiev@limechain.tech>
2021-11-04 16:54:30 +0200

    Add periodic vesting account

--- init.sh
+++ init.sh
@@ -10,6 +10,7 @@ KEYRING="test"
 CFG_DIR="$HOME/.cosmzone/config"
 
 VESTING_ACC="local-vesting-account"
+PERIODIC_VEST="periodic-vesting-account"
 
 update_config () {
   if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -21,6 +22,36 @@ update_config () {
 
 update_genesis () {
   jq "$1" < "$CFG_DIR/genesis.json" > "$CFG_DIR/tmp_genesis.json" && mv "$CFG_DIR/tmp_genesis.json" "$CFG_DIR/genesis.json"
+}
+
+create_periodic_vesting () {
+	local AMOUNT=$1 
+	local HP=$2
+	local S=$3
+	local N=$4
+
+	local P=$(expr $HP / $S)
+	local R=$(expr $HP % $S)
+
+	local PA=$(expr $AMOUNT / $S)
+	local RA=$(expr $AMOUNT % $S)	
+
+	if [ $R -gt 0 ]
+	then
+		echo 'Fix length' $P 
+		exit 1
+	fi
+
+	if [ $RA -gt 0 ]
+	then
+		echo 'Fix amount' $RA
+		exit 1
+	fi
+
+	for (( i=0; i<$S; i++ ))
+	do
+		jq --arg a $PA --argjson n $N --arg p $P --argjson i $i '.app_state["auth"]["accounts"][$n]["vesting_periods"][$i] = { "length": $p, "amount": [ { "amount": $a, "denom": "nomo" } ] }' < "$CFG_DIR/genesis.json" > "$CFG_DIR/tmp_genesis.json" && mv "$CFG_DIR/tmp_genesis.json" "$CFG_DIR/genesis.json"
+	done
 }
 
 # validate dependencies are installed
@@ -39,6 +70,7 @@ cosmzoned config chain-id $CHAINID
 # if $KEY exists it should be deleted
 cosmzoned keys add $KEY --keyring-backend $KEYRING
 cosmzoned keys add $VESTING_ACC --keyring-backend $KEYRING
+cosmzoned keys add $PERIODIC_VEST --keyring-backend $KEYRING
 
 cosmzoned init $MONIKER --chain-id $CHAINID
 
@@ -55,9 +87,22 @@ cosmzoned add-genesis-account $KEY 1000000000nomo --keyring-backend $KEYRING
 TILL=$(date -d "+10 minutes" +%s)
 cosmzoned add-genesis-account $VESTING_ACC 4325346435455nomo --vesting-amount 4325346435455nomo --vesting-end-time $TILL --keyring-backend $KEYRING
 
-# Add second DelayedVestingAccount
+# Add ContinuousVestingAccount
 TILL1H=$(date -d "+1 hours" +%s)
-cosmzoned add-genesis-account nomo1332gau6khc5xw5854ldsnd6vte4u6rl34j9s6v 1343144nomo --vesting-amount 1343144nomo --vesting-end-time $TILL1H
+cosmzoned add-genesis-account nomo1332gau6khc5xw5854ldsnd6vte4u6rl34j9s6v 1343144nomo --vesting-amount 1343144nomo --vesting-end-time $TILL1H --vesting-start-time $(date +%s)
+
+# Periodic Vesting Account
+
+TILL4H=$(date -d "+4 hours" +%s)
+PV1=546654
+LENGTH=14400
+PERIODS=4
+cosmzoned add-genesis-account $PERIODIC_VEST 1343144nomo --vesting-amount $PV1+nomo --vesting-end-time $TILL4H --vesting-start-time $(date +%s)
+
+update_genesis '.app_state["auth"]["accounts"][3]["@type"]="/cosmos.vesting.v1beta1.PeriodicVestingAccount"'
+update_genesis '.app_state["auth"]["accounts"][3]["vesting_periods"]+=[]'
+
+create_periodic_vesting $PV1 $LENGTH $PERIODS 3
 
 # Sign genesis transaction
 cosmzoned gentx $KEY 1000000nomo --keyring-backend $KEYRING --chain-id $CHAINID
