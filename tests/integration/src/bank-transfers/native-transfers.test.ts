868efef95cf569fdc25c2e90d5aba48a7a9996ac
Nikolay Stoyanov <n.stoyanov@nomo.io>
2021-11-08 16:48:45 +0200

    updated native transfers test

--- tests/integration/src/bank-transfers/native-transfers.test.ts
+++ tests/integration/src/bank-transfers/native-transfers.test.ts
@@ -1,43 +1,64 @@
 import { CosmWasmClient } from "@cosmjs/cosmwasm-stargate";
-import { assertIsBroadcastTxSuccess } from "@cosmjs/stargate";
-import { getValidatorClient, getValidatorWallet } from "../util/clients";
+import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
+import { DirectSecp256k1Wallet } from "@cosmjs/proto-signing";
+import { assertIsBroadcastTxSuccess, BroadcastTxResponse, Coin } from "@cosmjs/stargate";
+import { getValidatorClient, getValidatorWallet, getUser1Wallet, getUser2Wallet, getUser1Client } from "../util/clients";
 
-describe('native transfers', () => {
-    test('validator has positive balance', async () => {
-        const client = await CosmWasmClient.connect(process.env.NODE_URL as string);
-        const balance = await client.getBalance(process.env.VALIDATOR_ADDR as string, "nomo");
+describe("Native transfers", () => {
+    test("Validator has positive balance", async () => {
+        const client: CosmWasmClient = await CosmWasmClient.connect(process.env.NODE_URL as string);
+        const balance: Coin = await client.getBalance(process.env.VALIDATOR_ADDR as string, "nomo");
 
         console.log(`Validator balance=(${balance.denom}, ${balance.amount})`);
 
         expect(BigInt(balance.amount) > 0).toBeTruthy();
     });
 
-    test('validator can send tokens', async () => {
-        const wallet = await getValidatorWallet();
-        const client = await getValidatorClient();
-        const [firstAccount] = await wallet.getAccounts();
-        const amount = {
+    test("Users can transfer tokens", async () => {
+        const validatorClient: SigningCosmWasmClient = await getValidatorClient();
+        const user1Client: SigningCosmWasmClient = await getUser1Client();
+        const [validatorAccount] = await (await getValidatorWallet()).getAccounts();
+        const [user1Account] = await (await getUser1Wallet()).getAccounts();
+        const [user2Account] = await (await getUser2Wallet()).getAccounts();
+        const transfer1 = {
             denom: "nomo",
             amount: "1234",
         };
-        const feeAmount = "12";
+        const transfer2 = {
+            denom: "nomo",
+            amount: "1000",
+        };
         const fee = {
-            amount: [{denom: "nomo", amount: feeAmount}],
+            amount: [{denom: "nomo", amount: "12"}],
             gas: "100000"
         };
-        const previousValidatorBalance = await client.getBalance(firstAccount.address as string, "nomo");
-        const previousUsrBalance = await client.getBalance(process.env.USR_1_ADDR as string, "nomo");
-        const result = await client.sendTokens(firstAccount.address, process.env.USR_1_ADDR as string, [amount], fee, "Testing send transaction");
-        const nextValidatorBalance = await client.getBalance(firstAccount.address as string, "nomo");
-        const nextUsrBalance = await client.getBalance(process.env.USR_1_ADDR as string, "nomo");
 
-        console.log(`Validator balance before=(${previousValidatorBalance.denom}, ${previousValidatorBalance.amount})`);
-        console.log(`Validator balance after=(${nextValidatorBalance.denom}, ${nextValidatorBalance.amount})`);
-        console.log(`User balance before=(${previousUsrBalance.denom}, ${previousUsrBalance.amount})`);
-        console.log(`User balance after=(${nextUsrBalance.denom}, ${nextUsrBalance.amount})`);
+        // validator -> user1
+        let previousValidatorBalance: Coin = await validatorClient.getBalance(validatorAccount.address, "nomo");
+        let previousUser1Balance: Coin = await validatorClient.getBalance(user1Account.address, "nomo");
+        let broadcastTxResponse1: BroadcastTxResponse = await validatorClient.sendTokens(validatorAccount.address, user1Account.address, [transfer1], fee, "Testing send transaction");
+        let nextValidatorBalance: Coin = await validatorClient.getBalance(validatorAccount.address, "nomo");
+        let nextUser1Balance: Coin = await validatorClient.getBalance(user1Account.address, "nomo");
 
-        assertIsBroadcastTxSuccess(result);
-        expect(BigInt(nextUsrBalance.amount)).toBe(BigInt(previousUsrBalance.amount) + BigInt(amount.amount));
-        expect(BigInt(nextValidatorBalance.amount)).toBe(BigInt(previousValidatorBalance.amount) - BigInt(amount.amount) - BigInt(feeAmount));
+        console.log(`Validator balance before=(${previousValidatorBalance.denom}, ${previousValidatorBalance.amount}) after=(${nextValidatorBalance.denom}, ${nextValidatorBalance.amount})`);
+        console.log(`User1 balance before=(${previousUser1Balance.denom}, ${previousUser1Balance.amount}) after=(${nextUser1Balance.denom}, ${nextUser1Balance.amount})`);
+
+        assertIsBroadcastTxSuccess(broadcastTxResponse1);
+        expect(BigInt(nextValidatorBalance.amount)).toBe(BigInt(previousValidatorBalance.amount) - BigInt(transfer1.amount) - BigInt(fee.amount[0].amount));
+        expect(BigInt(nextUser1Balance.amount)).toBe(BigInt(previousUser1Balance.amount) + BigInt(transfer1.amount));
+
+        // user1 -> user2
+        previousUser1Balance = await validatorClient.getBalance(user1Account.address, "nomo");
+        let previousUser2Balance: Coin = await validatorClient.getBalance(user2Account.address, "nomo");
+        let broadcastTxResponse2: BroadcastTxResponse = await user1Client.sendTokens(user1Account.address, user2Account.address, [transfer2], fee, "Testing send transaction");
+        nextUser1Balance = await validatorClient.getBalance(user1Account.address, "nomo");
+        let nextUser2Balance: Coin = await validatorClient.getBalance(user2Account.address, "nomo");
+
+        console.log(`User1 balance before=(${previousUser1Balance.denom}, ${previousUser1Balance.amount}) after=(${nextUser1Balance.denom}, ${nextUser1Balance.amount})`);
+        console.log(`User2 balance before=(${previousUser2Balance.denom}, ${previousUser2Balance.amount}) after=(${nextUser2Balance.denom}, ${nextUser2Balance.amount})`);
+
+        assertIsBroadcastTxSuccess(broadcastTxResponse2);
+        expect(BigInt(nextUser1Balance.amount)).toBe(BigInt(previousUser1Balance.amount) - BigInt(transfer2.amount) - BigInt(fee.amount[0].amount));
+        expect(BigInt(nextUser2Balance.amount)).toBe(BigInt(previousUser2Balance.amount) + BigInt(transfer2.amount));
     });
 });
--- tests/integration/src/util/clients.ts
+++ tests/integration/src/util/clients.ts
@@ -4,6 +4,10 @@ import {fromHex} from "@cosmjs/encoding";
 
 let validatorWallet: DirectSecp256k1Wallet;
 let validatorClient: SigningCosmWasmClient;
+let user1Wallet: DirectSecp256k1Wallet;
+let user1Client: SigningCosmWasmClient;
+let user2Wallet: DirectSecp256k1Wallet;
+let user2Client: SigningCosmWasmClient;
 
 export async function getValidatorWallet(): Promise<DirectSecp256k1Wallet> {
     if (!validatorWallet) {
@@ -17,4 +21,32 @@ export async function getValidatorClient(): Promise<SigningCosmWasmClient> {
         validatorClient = await SigningCosmWasmClient.connectWithSigner(process.env.NODE_URL as string, await getValidatorWallet());
     }
     return validatorClient;
+}
+
+export async function getUser1Wallet(): Promise<DirectSecp256k1Wallet> {
+    if (!user1Wallet) {
+        user1Wallet = await DirectSecp256k1Wallet.fromKey(fromHex(process.env.USR_1_PRIV_KEY as string), "nomo");
+    }
+    return user1Wallet;
+}
+
+export async function getUser1Client(): Promise<SigningCosmWasmClient> {
+    if (!user1Client) {
+        user1Client = await SigningCosmWasmClient.connectWithSigner(process.env.NODE_URL as string, await getUser1Wallet());
+    }
+    return user1Client;
+}
+
+export async function getUser2Wallet(): Promise<DirectSecp256k1Wallet> {
+    if (!user2Wallet) {
+        user2Wallet = await DirectSecp256k1Wallet.fromKey(fromHex(process.env.USR_2_PRIV_KEY as string), "nomo");
+    }
+    return user2Wallet;
+}
+
+export async function getUser2Client(): Promise<SigningCosmWasmClient> {
+    if (!user2Client) {
+        user2Client = await SigningCosmWasmClient.connectWithSigner(process.env.NODE_URL as string, await getUser2Wallet());
+    }
+    return user2Client;
 }
