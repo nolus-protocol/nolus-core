d8a5d0918e77fa18cc6e41049d7824f140497ac0
Rusi Boyadjiev <rusiboyadjiev@gmail.com>
2021-12-13 13:03:10 +0200

    not completed protos
    Signed-off-by: Rusi Boyadjiev <rusiboyadjiev@gmail.com>

--- custom/auth/ante/sigverify.go
+++ custom/auth/ante/sigverify.go
@@ -3,11 +3,13 @@ package ante
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	type2 "gitlab-nomo.credissimo.net/nomo/cosmzone/x/suspend/types"
 )
 
 type NomoSuspendDecorator struct {
 	tk SuspendKeeper
 }
+
 func NewSuspendDecorator(tk SuspendKeeper) NomoSuspendDecorator {
 	return NomoSuspendDecorator{
 		tk: tk,
@@ -16,7 +18,17 @@ func NewSuspendDecorator(tk SuspendKeeper) NomoSuspendDecorator {
 
 func (mfd NomoSuspendDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
 	msgSuspend := mfd.tk.IsNodeSuspend(ctx)
+
 	if msgSuspend.Suspend {
+		includeSuspend := false
+		for _, msg := range tx.GetMsgs() {
+			if _, ok := msg.(*type2.MsgChangeSuspend); ok {
+				includeSuspend = true
+			}
+		}
+		if includeSuspend {
+			return next(ctx, tx, simulate)
+		}
 		return ctx, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "The node is suspended!")
 	}
 	return next(ctx, tx, simulate)
--- x/suspend/keeper/msg_server_change_suspend.go
+++ x/suspend/keeper/msg_server_change_suspend.go
@@ -7,13 +7,17 @@ import (
 	"gitlab-nomo.credissimo.net/nomo/cosmzone/x/suspend/types"
 )
 
-
-func (k msgServer) ChangeSuspend(goCtx context.Context,  msg *types.MsgChangeSuspend) (*types.MsgChangeSuspendResponse, error) {
+func (k msgServer) ChangeSuspend(goCtx context.Context, msg *types.MsgChangeSuspend) (*types.MsgChangeSuspendResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
+	//TODO if message is send from not admin account
+	if msg.Creator != msg.AdminKey {
+		return nil, goCtx.Err("")
+	}
+
 	k.SetNodeSuspend(ctx, msg)
-    // TODO: Handling the message
-    _ = ctx
+	// TODO: Handling the message
+	_ = ctx
 
 	return &types.MsgChangeSuspendResponse{}, nil
 }
