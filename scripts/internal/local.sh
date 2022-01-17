b7b8ea4bf9ad444ccbf5f638aa4149b39563c4da
Gancho Manev <g.manev@nolus.io>
2022-01-17 10:08:41 +0200

    [dev][net] configuring the nodes to find each other

--- scripts/internal/local.sh
+++ scripts/internal/local.sh
@@ -5,9 +5,10 @@ set -euxo pipefail
 local_root_dir=""
 local_chain_id=""
 # end "instance" variables
-LOCAL_BASE_RPC_PORT=26654
-LOCAL_BASE_P2P_PORT=26653
-LOCAL_BASE_PROXY_PORT=26655
+LOCAL_BASE_PORT=26653
+LOCAL_BASE_P2P_PORT=$((LOCAL_BASE_PORT))
+LOCAL_BASE_RPC_PORT=$((LOCAL_BASE_PORT+1))
+LOCAL_BASE_PROXY_PORT=$((LOCAL_BASE_PORT+2))
 
 init_local_sh() {
   local_root_dir="$1"
@@ -31,7 +32,19 @@ deploy() {
   update_app "$node_dir" '."grpc"."enable"' "false"
   update_app "$node_dir" '."grpc-web"."enable"' "false"
   update_config "$node_dir" '."rpc"."laddr"' '"tcp://0.0.0.0:'"$(rpc_port "$node_index")"'"'
-  update_config "$node_dir" '."p2p"."laddr"' '"tcp://0.0.0.0:'"$(p2p_port "$node_index")"'"'
+
+  local p2p_host_port
+  p2p_host_port=$(p2p_host_port "$node_index")
+  update_config "$node_dir" '."p2p"."laddr"' '"tcp://'"$p2p_host_port"'"'
+  update_config "$node_dir" '."p2p"."addr_book_strict"' 'false'
+  update_config "$node_dir" '."p2p"."allow_duplicate_ip"' 'true'
+
+  local p2p_first_node_id
+  p2p_first_node_id=$(tendermint_node_id "$node_id")
+  local p2p_first_host_port
+  p2p_first_host_port=$(p2p_host_port 1)
+  update_config "$node_dir" '."p2p"."persistent_peers"' '"'"$p2p_first_node_id@$p2p_first_host_port"'"'
+
   update_config "$node_dir" '."proxy_app"' '"tcp://127.0.0.1:'"$(proxy_port "$node_index")"'"'
 }
 
@@ -87,6 +100,11 @@ p2p_port() {
   port $LOCAL_BASE_P2P_PORT "$@"
 }
 
+p2p_host_port() {
+  local node_index=$1
+  echo "127.0.0.1:$(p2p_port "$node_index")"
+}
+
 proxy_port() {
   port $LOCAL_BASE_PROXY_PORT "$@"
 }
@@ -95,4 +113,12 @@ port() {
   local base_port=$1
   local index=$2
   echo $((base_port + index*3))
+}
+
+tendermint_node_id() {
+  local node_id="$1"
+  local node_dir
+  node_dir=$(node_dir "$node_id")
+
+  run_cmd "$node_dir" tendermint show-node-id
 }
