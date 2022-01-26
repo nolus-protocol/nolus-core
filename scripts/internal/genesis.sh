46715ac26012d1fe8f63f76ce86e6343ea771834
Gancho Manev <g.manev@nolus.io>
2022-01-26 18:06:42 +0200

    sourcing in the scripts requiring them

--- scripts/internal/genesis.sh
+++ scripts/internal/genesis.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+source "$SCRIPT_DIR"/cmd.sh
+
 # start "instance" variables
 genesis_home_dir=$(mktemp -d)
 # end "instance" variables
--- scripts/internal/local.sh
+++ scripts/internal/local.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+source "$SCRIPT_DIR"/cmd.sh
+
 # start "instance" variables
 local_root_dir=""
 local_chain_id=""
