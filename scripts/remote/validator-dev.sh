921ab476a1eb3d8f98c050da3febe90f8e812ce5
Gancho Manev <g.manev@nolus.io>
2022-02-14 16:46:51 +0200

    set correct TLS certificates

--- scripts/remote/validator-dev.sh
+++ scripts/remote/validator-dev.sh
@@ -38,8 +38,8 @@ HOST="127.0.0.1"
 P2P_PORT=$((base_port))
 RPC_PORT=$((base_port+1))
 API_PORT=$((base_port+3))
-TLS_CERT_FILE="/etc/pki/tls/certs/nolus.io.pem"
-TLS_KEY_FILE="/etc/pki/tls/private/nolus.io.key"
+TLS_CERT_FILE="/etc/pki/tls/certs/wildcard.nolus.io.pem"
+TLS_KEY_FILE="/etc/pki/tls/private/wildcard.nolus.io.key"
 
 rm -fr "$home_dir"
 mkdir -p "$home_dir"
