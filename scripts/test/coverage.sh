9e8c34fecee1d5a64d54cf0f4e2603cf779f372e
Nina Nikolova <n.nikolova@nolus.io>
2022-02-22 12:14:23 +0200

    Update coverage.sh

--- scripts/test/coverage.sh
+++ scripts/test/coverage.sh
@@ -9,7 +9,7 @@ go tool cover -func=coverage.txt ;
 # If needed, generate XML report
 if [ "$1" == "xml" ]; then
     go install github.com/boumenot/gocover-cobertura
-    $GOPATH/bin/gocover-cobertura < coverage.txt > coverage.xml
+    gocover-cobertura < coverage.txt > coverage.xml
 fi
 
 # If needed, generate HTML report
