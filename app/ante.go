ef5ac62d1038b0f4522495552d66b7a3ecbab9dd
Petr Antonov <pvantonov@corp.sputnik.ru>
2022-04-18 15:28:58 +0300

    Change code formatting

--- app/ante.go
+++ app/ante.go
@@ -1,71 +1,71 @@
 package app
 
 import (
-    wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
-    wasmTypes "github.com/CosmWasm/wasmd/x/wasm/types"
-    sdk "github.com/cosmos/cosmos-sdk/types"
-    sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-    "github.com/cosmos/cosmos-sdk/types/tx/signing"
-    "github.com/cosmos/cosmos-sdk/x/auth/ante"
-    authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
-    taxkeeper "gitlab-nomo.credissimo.net/nomo/cosmzone/x/tax/keeper"
-    taxtypes "gitlab-nomo.credissimo.net/nomo/cosmzone/x/tax/types"
+	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
+	wasmTypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	"github.com/cosmos/cosmos-sdk/x/auth/ante"
+	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
+	taxkeeper "gitlab-nomo.credissimo.net/nomo/cosmzone/x/tax/keeper"
+	taxtypes "gitlab-nomo.credissimo.net/nomo/cosmzone/x/tax/types"
 
-    "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/types"
 )
 
 // HandlerOptions are the options required for constructing a default SDK AnteHandler.
 type HandlerOptions struct {
-    AccountKeeper     ante.AccountKeeper
-    BankKeeper        taxtypes.BankKeeper
-    TaxKeeper         taxkeeper.Keeper
-    TxCounterStoreKey sdk.StoreKey
-    WasmConfig        wasmTypes.WasmConfig
-    SignModeHandler   authsigning.SignModeHandler
-    SigGasConsumer    func(meter sdk.GasMeter, sig signing.SignatureV2, params types.Params) error
+	AccountKeeper     ante.AccountKeeper
+	BankKeeper        taxtypes.BankKeeper
+	TaxKeeper         taxkeeper.Keeper
+	TxCounterStoreKey sdk.StoreKey
+	WasmConfig        wasmTypes.WasmConfig
+	SignModeHandler   authsigning.SignModeHandler
+	SigGasConsumer    func(meter sdk.GasMeter, sig signing.SignatureV2, params types.Params) error
 }
 
 func NewAnteHandler(options HandlerOptions) (sdk.AnteHandler, error) {
-    if options.AccountKeeper == nil {
-        return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "account keeper is required for ante builder")
-    }
+	if options.AccountKeeper == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "account keeper is required for ante builder")
+	}
 
-    if options.BankKeeper == nil {
-        return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "bank keeper is required for ante builder")
-    }
+	if options.BankKeeper == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "bank keeper is required for ante builder")
+	}
 
-    if options.SignModeHandler == nil {
-        return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "sign mode handler is required for ante builder")
-    }
+	if options.SignModeHandler == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrLogic, "sign mode handler is required for ante builder")
+	}
 
-    var sigGasConsumer = options.SigGasConsumer
-    if sigGasConsumer == nil {
-        sigGasConsumer = ante.DefaultSigVerificationGasConsumer
-    }
+	var sigGasConsumer = options.SigGasConsumer
+	if sigGasConsumer == nil {
+		sigGasConsumer = ante.DefaultSigVerificationGasConsumer
+	}
 
-    mempoolFeeDecorator := taxkeeper.NewMempoolFeeDecorator(options.TaxKeeper)
-    deductFeeDecorator := taxkeeper.NewDeductFeeDecorator(options.AccountKeeper, options.BankKeeper, options.TaxKeeper)
+	mempoolFeeDecorator := taxkeeper.NewMempoolFeeDecorator(options.TaxKeeper)
+	deductFeeDecorator := taxkeeper.NewDeductFeeDecorator(options.AccountKeeper, options.BankKeeper, options.TaxKeeper)
 
-    anteDecorators := []sdk.AnteDecorator{
-        ante.NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
-        wasmkeeper.NewLimitSimulationGasDecorator(options.WasmConfig.SimulationGasLimit), // after setup context to enforce limits early
-        wasmkeeper.NewCountTXDecorator(options.TxCounterStoreKey),
-        ante.NewRejectExtensionOptionsDecorator(),
-        //ante.NewMempoolFeeDecorator(), // we are replacing the original fee decorator with a custom one
-        mempoolFeeDecorator,
-        //NewMempoolFeeDecorator(options.TreasuryKeeper),
-        ante.NewValidateBasicDecorator(),
-        ante.NewTxTimeoutHeightDecorator(),
-        ante.NewValidateMemoDecorator(options.AccountKeeper),
-        ante.NewConsumeGasForTxSizeDecorator(options.AccountKeeper),
-        deductFeeDecorator,
-        //NewNomoDeductFeeDecorator(options.AccountKeeper, options.BankKeeper, options.TreasuryKeeper),
-        ante.NewSetPubKeyDecorator(options.AccountKeeper), // SetPubKeyDecorator must be called before all signature verification decorators
-        ante.NewValidateSigCountDecorator(options.AccountKeeper),
-        ante.NewSigGasConsumeDecorator(options.AccountKeeper, sigGasConsumer),
-        ante.NewSigVerificationDecorator(options.AccountKeeper, options.SignModeHandler),
-        ante.NewIncrementSequenceDecorator(options.AccountKeeper),
-    }
+	anteDecorators := []sdk.AnteDecorator{
+		ante.NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
+		wasmkeeper.NewLimitSimulationGasDecorator(options.WasmConfig.SimulationGasLimit), // after setup context to enforce limits early
+		wasmkeeper.NewCountTXDecorator(options.TxCounterStoreKey),
+		ante.NewRejectExtensionOptionsDecorator(),
+		//ante.NewMempoolFeeDecorator(), // we are replacing the original fee decorator with a custom one
+		mempoolFeeDecorator,
+		//NewMempoolFeeDecorator(options.TreasuryKeeper),
+		ante.NewValidateBasicDecorator(),
+		ante.NewTxTimeoutHeightDecorator(),
+		ante.NewValidateMemoDecorator(options.AccountKeeper),
+		ante.NewConsumeGasForTxSizeDecorator(options.AccountKeeper),
+		deductFeeDecorator,
+		//NewNomoDeductFeeDecorator(options.AccountKeeper, options.BankKeeper, options.TreasuryKeeper),
+		ante.NewSetPubKeyDecorator(options.AccountKeeper), // SetPubKeyDecorator must be called before all signature verification decorators
+		ante.NewValidateSigCountDecorator(options.AccountKeeper),
+		ante.NewSigGasConsumeDecorator(options.AccountKeeper, sigGasConsumer),
+		ante.NewSigVerificationDecorator(options.AccountKeeper, options.SignModeHandler),
+		ante.NewIncrementSequenceDecorator(options.AccountKeeper),
+	}
 
-    return sdk.ChainAnteDecorators(anteDecorators...), nil
+	return sdk.ChainAnteDecorators(anteDecorators...), nil
 }
