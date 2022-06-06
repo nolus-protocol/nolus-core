f7f44b5ab0c1bb9f0cb75dc1d99a58307bba1109
Petr Antonov <p.antonov@nolus.io>
2022-06-06 09:46:30 +0300

    Add tests for x/mint and remove multiplication by 1
    uncomment tests

---
+++ x/mint/genesis_test.go
@@ -0,0 +1,39 @@
+package mint_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	"gitlab-nomo.credissimo.net/nomo/cosmzone/app/params"
+	"gitlab-nomo.credissimo.net/nomo/cosmzone/testutil/nullify"
+	"gitlab-nomo.credissimo.net/nomo/cosmzone/testutil/simapp"
+	"gitlab-nomo.credissimo.net/nomo/cosmzone/x/mint"
+	"gitlab-nomo.credissimo.net/nomo/cosmzone/x/mint/types"
+)
+
+func TestGenesis(t *testing.T) {
+	params.SetAddressPrefixes()
+	app, err := simapp.TestSetup()
+	if err != nil {
+		t.Errorf("Error while creating simapp: %v\"", err)
+	}
+	blockTime := time.Now()
+	header := tmproto.Header{Height: app.LastBlockHeight() + 1}
+	ctx := app.BaseApp.NewContext(false, header).WithBlockTime(blockTime)
+	minterKeeper := app.MintKeeper
+
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+	}
+
+	acc := app.AccountKeeper
+	mint.InitGenesis(ctx, minterKeeper, acc, &genesisState)
+	got := mint.ExportGenesis(ctx, minterKeeper)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+}
