94c8e6d162788095842731c17b2a4e57e7d33bc8
Petr Antonov <p.antonov@nolus.io>
2022-06-21 08:24:54 +0300

    Include SendCoinsFromModuleToAccount

--- x/tax/types/expected_keepers.go
+++ x/tax/types/expected_keepers.go
@@ -17,5 +17,6 @@ type BankKeeper interface {
 	SpendableCoins(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
 	SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	// Methods imported from bank should be defined here
 }
