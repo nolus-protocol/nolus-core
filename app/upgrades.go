23b1871129cbdee097461c08283b14e1b49cf06b
ivivanov <i.ivanov@nolus.io>
2023-02-27 11:59:21 +0200

    feat: set updgrade handler for v0.2.0

--- app/upgrades.go
+++ app/upgrades.go
@@ -15,6 +15,7 @@ func (app *App) RegisterUpgradeHandlers() {
 
 	app.registerUpgradeV1_43(upgradeInfo)
 	app.registerUpgradeV1_44(upgradeInfo)
+	app.registerUpgradeV2_0(upgradeInfo)
 }
 
 // performs upgrade from v0.1.39 -> v0.1.43.
@@ -31,6 +32,15 @@ func (app *App) registerUpgradeV1_44(_ storetypes.UpgradeInfo) {
 	const UpgradeV1_44Plan = "v0.1.44"
 	app.UpgradeKeeper.SetUpgradeHandler(UpgradeV1_44Plan, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		ctx.Logger().Info("Upgrade handler execution", "name", UpgradeV1_44Plan)
+		return fromVM, nil
+	})
+}
+
+// performs upgrade from v0.1.43 -> v0.2.2.
+func (app *App) registerUpgradeV2_0(_ storetypes.UpgradeInfo) {
+	const UpgradeV2_0Plan = "v0.2.0"
+	app.UpgradeKeeper.SetUpgradeHandler(UpgradeV2_0Plan, func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		ctx.Logger().Info("Upgrade handler execution", "name", UpgradeV2_0Plan)
 		return fromVM, nil
 	})
 }
