98d57621cbe1cc54172c56f922b7ed0235878ce7
ivivanov <i.ivanov@nolus.io>
2023-05-26 16:28:42 +0300

    bug(scripts): fix safe dir remove

--- scripts/common/rm-dir.sh
+++ scripts/common/rm-dir.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-
 # Protects us from accidental directory deletes.
 
 rm_dir() {
     local dir_for_deletion="$1"
     local dir_final=$(basename ${dir_for_deletion})
-    local dir_main=$(dirname ${dir_for_deletion})
     local dir_we_delete=$(echo ${dir_final} | sed 's/\///g')
 
-    cd ${dir_main}
-
-    if [[ -d ${dir_we_delete} ]]; then
-        rm -rf ${dir_we_delete}
+    if [[ -n ${dir_we_delete} ]]; then
+        rm -rf ${dir_for_deletion}
     fi
 }
