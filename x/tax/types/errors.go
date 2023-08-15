ecae4fc70460d8aa4aadbfd6a8ae3754ab4cbccd
ivivanov <i.ivanov@nolus.io>
2023-08-15 11:49:26 +0300

    chore(lint): start using new errors module

--- x/tax/types/errors.go
+++ x/tax/types/errors.go
@@ -3,15 +3,15 @@ package types
 // DONTCOVER
 
 import (
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	errorsmod "cosmossdk.io/errors"
 )
 
 // x/tax module sentinel errors.
 var (
-	ErrInvalidFeeRate  = sdkerrors.Register(ModuleName, 1, "feeRate should be between 0 and 50")
-	ErrInvalidAddress  = sdkerrors.Register(ModuleName, 2, "invalid address")
-	ErrTooManyFeeCoins = sdkerrors.Register(ModuleName, 3, "only one fee denom per tx")
-	ErrInvalidFeeDenom = sdkerrors.Register(ModuleName, 4, "denom is not allowed")
-	ErrAmountNilOrZero = sdkerrors.Register(ModuleName, 5, "amount can not be nil or zero")
-	ErrInvalidTax      = sdkerrors.Register(ModuleName, 6, "tax can not be negative, zero or nil")
+	ErrInvalidFeeRate  = errorsmod.Register(ModuleName, 1, "feeRate should be between 0 and 50")
+	ErrInvalidAddress  = errorsmod.Register(ModuleName, 2, "invalid address")
+	ErrTooManyFeeCoins = errorsmod.Register(ModuleName, 3, "only one fee denom per tx")
+	ErrInvalidFeeDenom = errorsmod.Register(ModuleName, 4, "denom is not allowed")
+	ErrAmountNilOrZero = errorsmod.Register(ModuleName, 5, "amount can not be nil or zero")
+	ErrInvalidTax      = errorsmod.Register(ModuleName, 6, "tax can not be negative, zero or nil")
 )
