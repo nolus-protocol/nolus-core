e4350b0aeb70624fc908240d91447f9ceb9e7e0f
Lockwarr <denislav66@gmail.com>
2023-08-22 14:44:18 +0300

    wip: simulations

--- x/mint/simulation/decoder_test.go
+++ x/mint/simulation/decoder_test.go
@@ -1,50 +1,51 @@
 package simulation_test
 
-// import (
-// 	"fmt"
-// 	"testing"
+import (
+	"fmt"
+	"testing"
 
-// 	"github.com/Nolus-Protocol/nolus-core/custom/util"
-// 	"github.com/cosmos/cosmos-sdk/testutil/sims"
-// 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkmath "cosmossdk.io/math"
 
-// 	"github.com/stretchr/testify/require"
+	"github.com/Nolus-Protocol/nolus-core/custom/util"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
-// 	"github.com/Nolus-Protocol/nolus-core/x/mint/simulation"
-// 	"github.com/Nolus-Protocol/nolus-core/x/mint/types"
-// 	"github.com/cosmos/cosmos-sdk/types/kv"
-// )
+	"github.com/stretchr/testify/require"
 
-// refactor: fix when simulation refactor is done
-// func TestDecodeStore(t *testing.T) {
-// 	cdc := sims.MakeTestEncodingConfig().Marshaler
-// 	dec := simulation.NewDecodeStore(cdc)
+	"github.com/Nolus-Protocol/nolus-core/x/mint/simulation"
+	"github.com/Nolus-Protocol/nolus-core/x/mint/types"
+	"github.com/cosmos/cosmos-sdk/types/kv"
+	moduletestutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
+)
 
-// 	minter := types.NewMinter(sdk.MustNewDecFromStr("13.123456789"), sdk.NewUint(10003145), sdk.NewUint(uint64(util.GetCurrentTimeUnixNano())), sdk.ZeroUint())
+func TestDecodeStore(t *testing.T) {
+	cdc := moduletestutil.MakeTestEncodingConfig().Codec
+	dec := simulation.NewDecodeStore(cdc)
 
-// 	kvPairs := kv.Pairs{
-// 		Pairs: []kv.Pair{
-// 			{Key: types.MinterKey, Value: cdc.MustMarshal(&minter)},
-// 			{Key: []byte{0x99}, Value: []byte{0x99}},
-// 		},
-// 	}
-// 	tests := []struct {
-// 		name        string
-// 		expectedLog string
-// 	}{
-// 		{"Minter", fmt.Sprintf("%v\n%v", minter, minter)},
-// 		{"other", ""},
-// 	}
+	minter := types.NewMinter(sdk.MustNewDecFromStr("13.123456789"), sdk.NewUint(10003145), sdk.NewUint(uint64(util.GetCurrentTimeUnixNano())), sdkmath.ZeroUint())
 
-// 	for i, tt := range tests {
-// 		i, tt := i, tt
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			switch i {
-// 			case len(tests) - 1:
-// 				require.Panics(t, func() { dec(kvPairs.Pairs[i], kvPairs.Pairs[i]) }, tt.name)
-// 			default:
-// 				require.Equal(t, tt.expectedLog, dec(kvPairs.Pairs[i], kvPairs.Pairs[i]), tt.name)
-// 			}
-// 		})
-// 	}
-// }
+	kvPairs := kv.Pairs{
+		Pairs: []kv.Pair{
+			{Key: types.MinterKey, Value: cdc.MustMarshal(&minter)},
+			{Key: []byte{0x99}, Value: []byte{0x99}},
+		},
+	}
+	tests := []struct {
+		name        string
+		expectedLog string
+	}{
+		{"Minter", fmt.Sprintf("%v\n%v", minter, minter)},
+		{"other", ""},
+	}
+
+	for i, tt := range tests {
+		i, tt := i, tt
+		t.Run(tt.name, func(t *testing.T) {
+			switch i {
+			case len(tests) - 1:
+				require.Panics(t, func() { dec(kvPairs.Pairs[i], kvPairs.Pairs[i]) }, tt.name)
+			default:
+				require.Equal(t, tt.expectedLog, dec(kvPairs.Pairs[i], kvPairs.Pairs[i]), tt.name)
+			}
+		})
+	}
+}
--- x/tax/simulation/params_test.go
+++ x/tax/simulation/params_test.go
@@ -1,34 +1,33 @@
 package simulation_test
 
-// refactor: fix when simulation refactor is done
-// import (
-// 	"math/rand"
-// 	"testing"
+import (
+	"math/rand"
+	"testing"
 
-// 	"github.com/Nolus-Protocol/nolus-core/x/tax/simulation"
-// 	"github.com/stretchr/testify/require"
-// )
+	"github.com/Nolus-Protocol/nolus-core/x/tax/simulation"
+	"github.com/stretchr/testify/require"
+)
 
-// func TestParamChanges(t *testing.T) {
-// 	s := rand.NewSource(1)
-// 	r := rand.New(s)
+func TestParamChanges(t *testing.T) {
+	s := rand.NewSource(1)
+	r := rand.New(s)
 
-// 	expected := []struct {
-// 		composedKey string
-// 		key         string
-// 		simValue    string
-// 		subspace    string
-// 	}{
-// 		{"tax/FeeRate", "FeeRate", "35", "tax"},
-// 	}
+	expected := []struct {
+		composedKey string
+		key         string
+		simValue    string
+		subspace    string
+	}{
+		{"tax/FeeRate", "FeeRate", "35", "tax"},
+	}
 
-// 	paramChanges := simulation.ParamChanges(r)
-// 	require.Len(t, paramChanges, 1)
+	paramChanges := simulation.ParamChanges(r)
+	require.Len(t, paramChanges, 1)
 
-// 	for i, p := range paramChanges {
-// 		require.Equal(t, expected[i].composedKey, p.ComposedKey())
-// 		require.Equal(t, expected[i].key, p.Key())
-// 		require.Equal(t, expected[i].simValue, p.SimValue()(r))
-// 		require.Equal(t, expected[i].subspace, p.Subspace())
-// 	}
-// }
+	for i, p := range paramChanges {
+		require.Equal(t, expected[i].composedKey, p.ComposedKey())
+		require.Equal(t, expected[i].key, p.Key())
+		require.Equal(t, expected[i].simValue, p.SimValue()(r))
+		require.Equal(t, expected[i].subspace, p.Subspace())
+	}
+}
