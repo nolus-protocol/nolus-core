9da82441ba097dc035d9e1d1099732754659c870
Lockwarr <denislav66@gmail.com>
2023-08-22 14:44:18 +0300

    wip: simulations

--- x/tax/simulation/params.go
+++ x/tax/simulation/params.go
@@ -1,24 +1,23 @@
 package simulation
 
-// refactor: temporary, fix when simulation refactor is done
-// import (
-// 	"fmt"
-// 	"math/rand"
+import (
+	"fmt"
+	"math/rand"
 
-// 	"github.com/Nolus-Protocol/nolus-core/x/tax/types"
-// 	"github.com/cosmos/cosmos-sdk/x/simulation"
+	"github.com/Nolus-Protocol/nolus-core/x/tax/types"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
 
-// 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
-// )
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+)
 
-// // ParamChanges defines the parameters that can be modified by param change proposals
-// // on the simulation.
-// func ParamChanges(r *rand.Rand) []simtypes.ParamChange {
-// 	return []simtypes.ParamChange{
-// 		simulation.NewSimParamChange(types.ModuleName, string(types.KeyFeeRate),
-// 			func(r *rand.Rand) string {
-// 				return fmt.Sprint(GenRandomFeeRate(r))
-// 			},
-// 		),
-// 	}
-// }
+// ParamChanges defines the parameters that can be modified by param change proposals
+// on the simulation.
+func ParamChanges(r *rand.Rand) []simtypes.LegacyParamChange {
+	return []simtypes.LegacyParamChange{
+		simulation.NewSimLegacyParamChange(types.ModuleName, string(types.KeyFeeRate),
+			func(r *rand.Rand) string {
+				return fmt.Sprint(GenRandomFeeRate(r))
+			},
+		),
+	}
+}
