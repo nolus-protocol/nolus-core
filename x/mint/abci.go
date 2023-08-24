fc479daf13db4d72088e9b2a9fe38e12f4029a0d
Lockwarr <denislav66@gmail.com>
2023-08-24 16:35:19 +0300

    refactor: clean up vestings module

--- x/mint/abci.go
+++ x/mint/abci.go
@@ -192,11 +192,12 @@ func BeginBlocker(ctx sdk.Context, k keeper.Keeper) {
 		defer telemetry.ModuleSetGauge(types.ModuleName, float32(coinAmount.Uint64()), "minted_tokens")
 	}
 
-	ctx.EventManager().EmitEvent(
-		sdk.NewEvent(
-			types.EventTypeMint,
-			sdk.NewAttribute(types.AttributeKeyDenom, params.MintDenom),
-			sdk.NewAttribute(sdk.AttributeKeyAmount, coinAmount.String()),
-		),
-	)
+	// refactor: make sure event is emitted from the baseapp - https://github.com/cosmos/cosmos-sdk/blob/main/UPGRADING.md#all-2
+	// ctx.EventManager().EmitEvent(
+	// 	sdk.NewEvent(
+	// 		types.EventTypeMint,
+	// 		sdk.NewAttribute(types.AttributeKeyDenom, params.MintDenom),
+	// 		sdk.NewAttribute(sdk.AttributeKeyAmount, coinAmount.String()),
+	// 	),
+	// )
 }
