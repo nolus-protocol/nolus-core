52e23e0532a452a6926e052df9502a48426c31bb
ivivanov <i.ivanov@nolus.io>
2023-11-28 17:51:32 +0200

    feat: set upgrade handler for v0.4.2

---
+++ app/upgrades/v042/constants.go
@@ -0,0 +1,22 @@
+package v042
+
+import (
+	"github.com/Nolus-Protocol/nolus-core/app/upgrades"
+	store "github.com/cosmos/cosmos-sdk/store/types"
+	"github.com/cosmos/cosmos-sdk/x/authz"
+)
+
+const (
+	// UpgradeName defines the on-chain upgrades name.
+	UpgradeName = "v0.4.2-c"
+)
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: store.StoreUpgrades{
+		Added: []string{
+			authz.ModuleName,
+		},
+	},
+}
---
+++ app/upgrades/v042/upgrades.go
@@ -0,0 +1,19 @@
+package v042
+
+import (
+	"github.com/Nolus-Protocol/nolus-core/app/keepers"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	keepers *keepers.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		return mm.RunMigrations(ctx, configurator, fromVM)
+	}
+}
