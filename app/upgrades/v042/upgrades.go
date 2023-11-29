003a9fac10cc5d0aa59979c1a932a9b69e87755a
Lockwarr <denislav66@gmail.com>
2023-11-29 12:40:08 +0200

    fix: errors after rebase

--- app/upgrades/v042/upgrades.go
+++ app/upgrades/v042/upgrades.go
@@ -3,6 +3,7 @@ package v042
 import (
 	"github.com/Nolus-Protocol/nolus-core/app/keepers"
 
+	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
@@ -12,6 +13,7 @@ func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
 	keepers *keepers.AppKeepers,
+	codec codec.Codec,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 		return mm.RunMigrations(ctx, configurator, fromVM)
