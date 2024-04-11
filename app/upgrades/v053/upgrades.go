59dbe8c5878db26851213a78f50c1ef14c5ee44c
Lockwarr <denislav66@gmail.com>
2024-04-11 10:13:00 +0300

    fix: errors after rebase w main

--- app/upgrades/v053/upgrades.go
+++ app/upgrades/v053/upgrades.go
@@ -1,12 +1,14 @@
 package v051
 
 import (
+	"context"
+
 	"github.com/Nolus-Protocol/nolus-core/app/keepers"
 
+	upgradetypes "cosmossdk.io/x/upgrade/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
 func CreateUpgradeHandler(
@@ -15,7 +17,9 @@ func CreateUpgradeHandler(
 	keepers *keepers.AppKeepers,
 	codec codec.Codec,
 ) upgradetypes.UpgradeHandler {
-	return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+	return func(c context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		ctx := sdk.UnwrapSDKContext(c)
+
 		ctx.Logger().Info(`IBC-Go v7.4.0 upgrade handler execution...`)
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
