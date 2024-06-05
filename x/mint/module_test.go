6afb8ab002bbf2b26fbb74afa12221ebd10fc42c
Lockwarr <denislav66@gmail.com>
2024-06-05 14:48:20 +0300

    chore: fix some todos

--- x/mint/module_test.go
+++ x/mint/module_test.go
@@ -1,11 +1,22 @@
 package mint_test
 
-// TODO: fix
-// func TestItCreatesModuleAccountOnInitBlock(t *testing.T) {
-// 	// simapp.Setup initializes the app
-// 	app := simapp.Setup(t, false)
-// 	ctx := app.BaseApp.NewContext(false)
+import (
+	"testing"
 
-// 	acc := app.AccountKeeper.GetAccount(ctx, authtypes.NewModuleAddress(types.ModuleName))
-// 	require.NotNil(t, acc)
-// }
+	"github.com/Nolus-Protocol/nolus-core/testutil/simapp"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/Nolus-Protocol/nolus-core/x/mint/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+)
+
+func TestItCreatesModuleAccountOnInitBlock(t *testing.T) {
+	// simapp.TestSetup initializes the app
+	app, err := simapp.TestSetup(t)
+	require.NoError(t, err)
+	ctx := app.BaseApp.NewContext(false)
+
+	acc := app.AccountKeeper.GetAccount(ctx, authtypes.NewModuleAddress(types.ModuleName))
+	require.NotNil(t, acc)
+}
