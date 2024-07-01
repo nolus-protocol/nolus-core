51398ebf32c12aec22177612794fbe57c1a2e007
Lockwarr <denislav66@gmail.com>
2024-07-01 13:28:28 +0300

    fix: fix creation of vesting accounts through our custom module

--- x/vestings/types/codec.go
+++ x/vestings/types/codec.go
@@ -2,19 +2,23 @@ package types
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/legacy"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
-	cdc.RegisterConcrete(&MsgCreateVestingAccount{}, "vestings/CreateVestingAccount", nil)
+	cdc.RegisterConcrete(&MsgCreateVestingAccount{}, "nolus/x/vestings/CreateVestingAccount", nil)
+	legacy.RegisterAminoMsg(cdc, &MsgCreateVestingAccount{}, "nolus-core/x/vestings/CreateVestingAccount")
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-	registry.RegisterImplementations((*sdk.Msg)(nil),
+	registry.RegisterImplementations(
+		(*sdk.Msg)(nil),
 		&MsgCreateVestingAccount{},
 	)
+
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
 
