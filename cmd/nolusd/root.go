92f640969ef842bc77ecf18e2a8d93bc3717ef88
Lockwarr <denislav66@gmail.com>
2024-08-06 14:41:20 +0300

    wip: remove unused code

--- cmd/nolusd/root.go
+++ cmd/nolusd/root.go
@@ -270,8 +270,6 @@ func initRootCmd(
 		),
 		genutilcli.ValidateGenesisCmd(app.ModuleBasics),
 		addGenesisAccountCmd(app.DefaultNodeHome),
-		// TODO: remove?
-		// addGenesisWasmMsgCmd(app.DefaultNodeHome),
 		tmcli.NewCompletionCmd(rootCmd, true),
 		debug.Cmd(),
 		confixcmd.ConfigCommand(),
