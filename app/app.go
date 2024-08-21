b500853d49aa447fbf5fdf4360954186e8b35578
Lockwarr <denislav66@gmail.com>
2024-08-21 16:30:14 +0300

    fix: upgrade handler v063

--- app/app.go
+++ app/app.go
@@ -51,7 +51,7 @@ import (
 	"github.com/Nolus-Protocol/nolus-core/app/keepers"
 	appparams "github.com/Nolus-Protocol/nolus-core/app/params"
 	"github.com/Nolus-Protocol/nolus-core/app/upgrades"
-	v062 "github.com/Nolus-Protocol/nolus-core/app/upgrades/v062"
+	v063 "github.com/Nolus-Protocol/nolus-core/app/upgrades/v063"
 	"github.com/Nolus-Protocol/nolus-core/docs"
 
 	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
@@ -67,7 +67,7 @@ const (
 var (
 	DefaultNodeHome string
 
-	Upgrades = []upgrades.Upgrade{v062.Upgrade}
+	Upgrades = []upgrades.Upgrade{v063.Upgrade}
 )
 
 var (
