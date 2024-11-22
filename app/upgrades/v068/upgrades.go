be1bcbc8832f2bdb74202c031ba6c2b920755e9b
Lockwarr <denislav66@gmail.com>
2024-11-22 10:52:26 +0200

    feat: v0.6.8

---
+++ app/upgrades/v068/upgrades.go
@@ -0,0 +1,33 @@
+package v068
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Nolus-Protocol/nolus-core/app/keepers"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	keepers *keepers.AppKeepers,
+	codec codec.Codec,
+) upgradetypes.UpgradeHandler {
+	return func(c context.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		ctx := sdk.UnwrapSDKContext(c)
+
+		ctx.Logger().Info("Starting module migrations...")
+		vm, err := mm.RunMigrations(ctx, configurator, vm) //nolint:contextcheck
+		if err != nil {
+			return vm, err
+		}
+
+		ctx.Logger().Info(fmt.Sprintf("Migration {%s} applied", UpgradeName))
+		return vm, nil
+	}
+}
