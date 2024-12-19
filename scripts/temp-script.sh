2346d4ccdbf4f5874f53590f17793f64e56a63c7
Lockwarr <denislav66@gmail.com>
2024-12-19 15:50:00 +0200

    wip: v0.6.9 release

--- scripts/temp-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-USER_DIR="/home/denislavivanov/.nolus"
-WASM_CODE_ARTIFACTS_PATH_PLATFORM="/home/denislavivanov/go/github/nolus-money-market/artifacts-v0.7.1-dev/platform"
-
-source "/home/denislavivanov/go/github/nolus-money-market/scripts/deploy-platform.sh"
-
-deploy_contracts "$USER_DIR" "$WASM_CODE_ARTIFACTS_PATH_PLATFORM" "1000000000unls" "nolus1unzvj963cha6gfthjcxj5me6cmzr0w6yw3td7d" "nolus18rl557k4jcjjcx7wmcrw47wkg25e3k3vmata54"
