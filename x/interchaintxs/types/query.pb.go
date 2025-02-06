92d32a8e97b955df383c2ef9fd1ed81e42ed934e
Lockwarr <denislav66@gmail.com>
2025-02-06 17:11:39 +0200

    fix: remove deprecated ibc queries and regenerate proto files with latest base image

--- x/interchaintxs/types/query.pb.go
+++ x/interchaintxs/types/query.pb.go
@@ -351,6 +351,7 @@ func _Query_InterchainAccountAddress_Handler(srv interface{}, ctx context.Contex
 	return interceptor(ctx, in, info, handler)
 }
 
+var Query_serviceDesc = _Query_serviceDesc
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "nolus.interchaintxs.v1.Query",
 	HandlerType: (*QueryServer)(nil),
