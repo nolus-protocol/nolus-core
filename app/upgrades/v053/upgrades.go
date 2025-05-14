3ba13c8d1b464b32579ee2dab35cb7e242c34b67
Lockwarr <denislav66@gmail.com>
2025-05-14 10:18:03 +0300

    fix: wire older upgrade handlers

---
+++ app/upgrades/v053/upgrades.go
@@ -0,0 +1,26 @@
+package v053
+
+import (
+	"context"
+
+	"github.com/Nolus-Protocol/nolus-core/app/keepers"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	keepers *keepers.AppKeepers,
+	codec codec.Codec,
+) upgradetypes.UpgradeHandler {
+	return func(c context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		ctx := sdk.UnwrapSDKContext(c)
+
+		ctx.Logger().Info(`IBC-Go v7.4.0 upgrade handler execution...`)
+		return mm.RunMigrations(ctx, configurator, fromVM) //nolint:contextcheck
+	}
+}
