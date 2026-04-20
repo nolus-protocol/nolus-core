a022e1a6f564be1700f19dfc7f9b20ba7427a368
Lockwarr <denislav66@gmail.com>
2026-04-20 11:46:52 +0300

    wip: regenerate proto files

--- x/interchaintxs/types/tx.pb.go
+++ x/interchaintxs/types/tx.pb.go
@@ -6,10 +6,6 @@ package types
 import (
 	context "context"
 	fmt "fmt"
-	io "io"
-	math "math"
-	math_bits "math/bits"
-
 	types2 "github.com/Nolus-Protocol/nolus-core/x/feerefunder/types"
 	_ "github.com/cosmos/cosmos-proto"
 	types1 "github.com/cosmos/cosmos-sdk/codec/types"
@@ -23,6 +19,9 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	io "io"
+	math "math"
+	math_bits "math/bits"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
